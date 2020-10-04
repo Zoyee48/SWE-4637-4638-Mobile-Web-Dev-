00d3e4125ab0e522ad8cac0f69dcd396895dca89
malihazoyee48 <malihamehzabin@iut-dhaka.edu>
2020-10-04 23:13:16 +0600

    implemented a second page.

--- first_project/App.js
+++ first_project/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-//import HomeScreen from "./src/screens/HomeScreen";
-import anotherScreen from "./src/screens/anotherScreen";
+import HomeScreen from "./src/screens/HomeScreen";
+import ListScreen from "./src/screens/ListScreen";
 const stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
       <stack.Navigator initialRouteName="Home">
-        <stack.Screen name="Home" component={anotherScreen} />
+        <stack.Screen name="Home" component={HomeScreen} />
+        <stack.Screen name="List" component={ListScreen} />
       </stack.Navigator>
     </NavigationContainer>
   );
--- first_project/src/screens/HomeScreen.js
+++ first_project/src/screens/HomeScreen.js
@@ -1,19 +1,27 @@
 import React from "react";
-import { Text, StyleSheet} from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
-const HomeScreen = () =>
-{
-    return (<Text style={styles.textStyle}> Hi There! </Text>);
-}
+const HomeScreen = (props) => {
+  //console.log(props);
+  return (
+    <View>
+      <Text style={styles.textStyle}>HomeScreen</Text>
+      <Button
+        title="List Screen"
+        onPress={function () {
+          props.navigation.navigate("List");
+          console.log("Button Pressed");
+        }}
+      />
+    </View>
+  );
+};
 
-const styles = StyleSheet.create(
-    {
-        textStyle:{
-            fontSize: 30,
-            color: 'blue',
-        },
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 30,
+    color: "blue",
+  },
+});
 
-    }
-);
-
-export default HomeScreen;   
+export default HomeScreen;
