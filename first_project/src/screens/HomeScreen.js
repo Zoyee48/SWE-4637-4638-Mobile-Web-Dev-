172fae38283e79ea37af5bd4f0aa7575d454f9f5
malihazoyee48 <malihamehzabin@iut-dhaka.edu>
2020-10-05 00:46:12 +0600

    image added.

--- first_project/src/screens/HomeScreen.js
+++ first_project/src/screens/HomeScreen.js
@@ -1,12 +1,17 @@
 import React from "react";
-import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity, Image} from "react-native";
 
 const HomeScreen = (props) => {
   //console.log(props);
   return (
     <View>
+
+      <Image source = {require("./../../assets/icon.png")}>
+        
+      </Image>
       <Text style={styles.textStyle}>Department Of CSE</Text>
       <Text style={styles.textStyle}>Programme: SWE</Text>
+      <Text style={styles.textStyle}></Text>
       <TouchableOpacity>
       <Button
         title="My Profile"
@@ -17,12 +22,17 @@ const HomeScreen = (props) => {
       />
       </TouchableOpacity>
 
-       <Button
+      <Text style={styles.textStyle}></Text>
+
+      <Button
         title="Semester Wise Course List"
         onPress={function () {
           console.log("Button Pressed");
         }}
       />
+
+      <Text style={styles.textStyle}></Text>
+
       <Button
         title="List of Faculty Members"
         onPress={function () {
@@ -37,6 +47,7 @@ const styles = StyleSheet.create({
   textStyle: {
     fontSize: 30,
     color: "blue",
+    textAlign: "center",
   },
 });
 
